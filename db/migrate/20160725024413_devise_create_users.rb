8bdad095c459e4dc4d12e5119024f34395014030
MAURICIO DURAN <mauriciodurant@gmail.com>
2016-07-24 23:25:34 -0400

    mejora en modelo, tabla y otros para recuperar password

--- db/migrate/20160725024413_devise_create_users.rb
+++ db/migrate/20160725024413_devise_create_users.rb
@@ -1,6 +1,7 @@
 class DeviseCreateUsers < ActiveRecord::Migration
+
   def change
-    create_table :users do |t|
+    create_table(:users) do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
@@ -25,13 +26,12 @@ class DeviseCreateUsers < ActiveRecord::Migration
       # t.datetime :confirmation_sent_at
       # t.string   :unconfirmed_email # Only if using reconfirmable
 
-      ## Lockable
+      # Lockable
       # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-
-      t.timestamps null: false
+      t.timestamps
     end
 
     add_index :users, :email,                unique: true
